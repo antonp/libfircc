89bbce804fd3d08a1f135496c6765837f19d6eb9
Anton Petersson <anton-vcs@anp.nu>
2009-12-16 14:29:25 +0100

    Added ChannelCache stub.

---
+++ core/modules/cache/channelcache.h
@@ -0,0 +1,16 @@
+#ifndef _CHANNELCACHE_H_
+#define _CHANNELCACHE_H_
+
+namespace anp
+{
+namespace firc
+{
+	class ChannelCache
+	{
+	public:
+		int removeMe;
+	};
+}
+}
+
+#endif // _CHANNELCACHE_H_

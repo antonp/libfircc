4598f7bdd7a4a6738f0f9789435738c4c0a5a7a2
Anton Petersson <anton-vcs@anp.nu>
2009-12-16 16:07:29 +0100

    const method to retrieve channels.

---
+++ core/modules/cache/src/channelcache.cpp
@@ -0,0 +1,14 @@
+#include <basedefs.h>
+#include <string>
+#include "../channelcache.h"
+
+namespace anp
+{
+namespace firc
+{
+	const std::string &ChannelCache::name() const
+	{
+		return std::string("todo"); //m_name;
+	}
+}
+}

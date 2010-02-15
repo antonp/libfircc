d0ef3320e6cacb5091b12918d24cb9321bbaf6f6
Anton Petersson <anton-vcs@anp.nu>
2010-02-15 20:13:34 +0100

    Documentation.
    Added documentation for INetworkManagerFrontend and
    INetworkCacheUser.

--- core/modules/cache/networkcache_userinterface.h
+++ core/modules/cache/networkcache_userinterface.h
@@ -12,10 +12,40 @@ namespace firc
 	public:
 		virtual ~NetworkCacheUserInterface() { }
 
+		/**
+		Returns a ChannelCache which can be used to retrieve
+		information about a particular channel.
+
+		@param name
+		The name of the channel.
+
+		@return
+		Pointer to ChannelCache.
+		*/
 		virtual const ChannelCache *getChannel(
 									const std::string &name) const = 0;
+
+		/**
+		Copy information about a particular channel into an
+		already allocated ChannelCache object.
+
+		@param name
+		The name of the channel.
+
+		@param dest
+		The channel information will be written to this ChannelCache
+		object.
+		*/
 		virtual void getChannel(const std::string &name,
 										ChannelCache &dest) const = 0;
+
+		/**
+		Copies the client nick name into the clientNickName
+		parameter.
+
+		@param clientNickName
+		The client nickname will be written to this string.
+		*/
 		virtual void getClientNickName(std::string &clientNickName)
 															const = 0;
 	};
--- core/modules/networkmanager/networkmanager_frontend.h
+++ core/modules/networkmanager/networkmanager_frontend.h
@@ -17,32 +17,71 @@ namespace firc
 		INetworkManagerFrontend() { }
 		virtual ~INetworkManagerFrontend() { }
 
+		/**
+		Sends the QUIT message to IRC and blocks until the receiver
+		thread (if any) has exited.
+
+		@param message
+		The quit message to send to the IRC network.
+		*/
 		virtual void deinit(const std::string &message) = 0;
 
+		/**
+		Sends a raw message to the IRC server.
+
+		@param message
+		The message to send. Should include the trailing CRLF as
+		it is not added automatically by this method.
+		*/
 		virtual void sendMessage(const std::string &message) = 0;
 
+		/**
+		Spawns a thread that calls runMessageReceiver.
+		*/
 		virtual void runMessageReceiverInThread() = 0;
 		
 		// Network cache stuff (getCache() instead ?)
 
+		/**
+		Returns a reference to a NetworkCacheUserInterface which
+		can be used to query the internal database for information
+		about channels and users, but not to modify it.
+		*/
 		virtual const NetworkCacheUserInterface &networkCache() const = 0;
 
+		/**
+		Returns a subscriber interface for the JOIN event dispatcher.
+		*/
 		virtual IEventDispatcherSubscriber
 		<events::ISubscriber<events::Join> > &
 		eventDispatcherJoin() = 0;
 
+		/**
+		Returns a subscriber interface for the PART event dispatcher.
+		*/
 		virtual IEventDispatcherSubscriber
 		<events::ISubscriber<events::Part> > &
 		eventDispatcherPart() = 0;
 
+		/**
+		Returns a subscriber interface for the PRIVMSG event dispatcher.
+		*/
 		virtual IEventDispatcherSubscriber
 		<events::ISubscriber<events::PrivMsg> > &
 		eventDispatcherPrivMsg() = 0;
 
+		/**
+		Returns a subscriber interface for the TOPIC event dispatcher.
+		*/
 		virtual IEventDispatcherSubscriber
 		<events::ISubscriber<events::Topic> > &
 		eventDispatcherTopic() = 0;
 
+		/**
+		Returns a subscriber interface for the numeric reply event
+		dispatcher, which is a generic dispatcher for all numeric
+		replies.
+		*/
 		virtual IEventDispatcherSubscriber
 		<events::ISubscriber<events::NumericReply> > &
 		eventDispatcherNumericReply() = 0;

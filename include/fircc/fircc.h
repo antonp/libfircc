4ea731105e43cf549f9c54acfe0eb8c335968693
Anton Petersson <anton@anp.nu>
2012-08-15 15:46:17 +0200

    Add convenience header (gh-5)

---
+++ include/fircc/fircc.h
@@ -0,0 +1,46 @@
+/*
+
+Copyright (c) 2012, Anton Petersson < anton (at) anp.nu >
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+/*
+This is just a convenience header to allow applications
+to use the lib simply by including this single header.
+*/
+
+#ifndef FIRCC_H_
+#define FIRCC_H_
+
+#include "inetwork.h"
+#include "messageprefix.h"
+#include "networkcache_userinterface.h"
+#include "channelcache.h"
+#include "networkeventdispatchers.h"
+#include "networkevents.h"
+#include "networkeventsubscribers.h"
+#include "networkfactory.h"
+#include "pluginmanager.h"
+
+#endif // FIRCC_H_
